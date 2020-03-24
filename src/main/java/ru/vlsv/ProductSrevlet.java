bb5c6426c9048193b5ad79d7075493d4b1d9b616
CentnerMan <44133485+CentnerMan@users.noreply.github.com>
2020-03-25 00:48:28 +0300

    lesson 2

---
+++ src/main/java/ru/vlsv/ProductSrevlet.java
@@ -0,0 +1,12 @@
+package ru.vlsv;
+
+/**
+ * GeekBrains Java, JavaEEWebApp.
+ *
+ * @author Anatoly Lebedev
+ * @version 1.0.0 24.03.2020
+ * @link https://github.com/Centnerman
+ */
+
+public class ProductSrevlet {
+}
---
+++ src/main/java/ru/vlsv/filters/HTTPFilter.java
@@ -0,0 +1,12 @@
+package ru.vlsv.filters;
+
+/**
+ * GeekBrains Java, JavaEEWebApp.
+ *
+ * @author Anatoly Lebedev
+ * @version 1.0.0 25.03.2020
+ * @link https://github.com/Centnerman
+ */
+
+public class HTTPFilter {
+}
