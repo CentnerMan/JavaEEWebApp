bbfe5bdb432c5b3a04a13cdacd10bcf7ace19184
CentnerMan <44133485+CentnerMan@users.noreply.github.com>
2020-06-28 22:14:52 +0300

    hw-5 added category

---
+++ src/main/java/ru/vlsv/entity/ProductDTO.java
@@ -0,0 +1,12 @@
+package ru.vlsv.entity;
+
+/**
+ * GeekBrains Java, JavaEEWebApp.
+ *
+ * @author Anatoly Lebedev
+ * @version 1.0.0 28.06.2020
+ * @link https://github.com/Centnerman
+ */
+
+public class ProductDTO {
+}
